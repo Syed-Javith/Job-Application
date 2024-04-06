cae5d5e21d43344548470d9b9322557c17af7598
Syed-Javith <syedjavith14@gmail.com>
2024-04-06 09:22:55 +0530

    Updated

--- ML/model.py
+++ ML/model.py
@@ -5,8 +5,8 @@ from sklearn.preprocessing import MultiLabelBinarizer, OneHotEncoder
 from sklearn.preprocessing import MinMaxScaler
 from utils import haversine, get_lat_lon,generate_recommendations
 from pymongo import MongoClient
-from sklearn.feature_extraction.text import CountVectorizer
-
+from sklearn.feature_extraction.text import CountVectorizer , TfidfVectorizer
+from ast import literal_eval
 def recommend_jobs(current_user='66090c577048d5d007b0261b'):
     client = MongoClient('mongodb://localhost:27017/')  
     db = client['job-db']  
@@ -25,6 +25,23 @@ def recommend_jobs(current_user='66090c577048d5d007b0261b'):
     users = pd.read_csv('users.csv')
     job_posts = pd.read_csv('jobposts.csv')
 
+    users['skills'] = users['skills'].apply(lambda x : " ".join(literal_eval(x)))
+    job_posts['skillsRequired'] = job_posts['skillsRequired'].apply(lambda x : " ".join(literal_eval(x)))
+
+    user_skills = users[users[ '_id' ] == current_user]['skills'].values[0].lower()
+    job_descriptions =  job_posts['skillsRequired'].str.lower().tolist()
+    
+
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform([user_skills] + job_descriptions)
+
+    # Get user skills vector
+    user_vector = tfidf_matrix[0]
+
+    # Calculate cosine similarity between user and jobs
+    similarity_matrix= cosine_similarity(user_vector.reshape(1, -1), tfidf_matrix[1:])
+
+    """"
     # ////////// #
     users['skills'] = users['skills'].apply(lambda x: eval(x))
     job_posts['skillsRequired'] = job_posts['skillsRequired'].apply(lambda x: eval(x))
@@ -34,8 +51,8 @@ def recommend_jobs(current_user='66090c577048d5d007b0261b'):
     job_post_skills_encoded = pd.DataFrame(mlb.transform(job_posts['skillsRequired']), columns=mlb.classes_, index=job_posts.index)
 
     # ////////// #
-
-    similarity_matrix= cosine_similarity(user_skills_encoded, job_post_skills_encoded)
+    """
+    # similarity_matrix= cosine_similarity(user_skills_encoded, job_post_skills_encoded)
 
 
     job_posts['lat'] = job_posts['location'].apply(lambda x : get_lat_lon(x)[0])
@@ -57,6 +74,8 @@ def recommend_jobs(current_user='66090c577048d5d007b0261b'):
             print(f"{job_post_id}: {job_post_title}")
             recommend_jobs_id_list.append(job_post_id)
     return recommend_jobs_id_list
-recommend_jobs()
+recommend_jobs(
+'6607bb049555c10badf7e468'
+)
 
 # '6607bb049555c10badf7e468'
